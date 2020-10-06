a39d26a5658ba0930ec80332932d0068b44777fd
leeyelan <yr00868@sookmyung.ac.kr>
2020-10-06 15:36:05 +0900

    1st

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	printf("Input a number:");
+	scanf("%i", &a);
+	
+	if (a > 0)
+
+	
+     printf("positive number.\n");
+	
+	
+	else if (a < 0)
+
+	
+	printf("negative number.\n");
+	 
+
+    else
+    printf("Zero.\n");
+
+	
+	
+	
+	return 0;
+	
+}
